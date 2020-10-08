44cc78d7a26d5d779e4f06e301365dd04d368f01
Daniel3424 <kwnjh3424@gmail.com>
2020-10-08 22:13:16 +0200

    Updated_20201008

---
+++ 844. Backspace String Compare.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## 844. Backspace String Compare
+# 
+# Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+# 
+# Note that after backspacing an empty text, the text will continue empty.
+# 
+# Example 1:
+# ```
+# Input: S = "ab#c", T = "ad#c"
+# Output: true
+# Explanation: Both S and T become "ac".
+# ```
+# Example 2:
+# ```
+# Input: S = "ab##", T = "c#d#"
+# Output: true
+# Explanation: Both S and T become "".
+# ```
+# Example 3:
+# ```
+# Input: S = "a##c", T = "#a#c"
+# Output: true
+# Explanation: Both S and T become "c".
+# ```
+# Example 4:
+# ```
+# Input: S = "a#c", T = "b"
+# Output: false
+# Explanation: S becomes "c" while T becomes "b".
+# ```
+# Note:
+# 
+# 1 <= S.length <= 200
+# 1 <= T.length <= 200
+# S and T only contain lowercase letters and '#' characters.
+# Follow up:
+# 
+# Can you solve it in O(N) time and O(1) space?
+
+# In[ ]:
+
+
+class Solution(object):
+    def backspaceCompare(self, S, T):
+        """
+        :type S: str
+        :type T: str
+        :rtype: bool
+        """
+        list1 = []
+        list2 = []
+        
+        for letter in S:
+          if letter == "#":
+            if list1:
+              list1.pop()
+          else:
+            list1.append(letter)
+            
+        for letter in T:
+          if letter == "#":
+            if list2:
+              list2.pop()
+          else:
+            list2.append(letter)
+            
+        return list1 == list2
+
